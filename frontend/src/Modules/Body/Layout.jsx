98118f9f68302601e1b4bfea0120074d8c84b5a3
awaismalik01 <mawais_malik@hotmail.com>
2023-05-09 00:35:48 -0700

    Maintanence

--- frontend/src/Modules/Body/Layout.jsx
+++ frontend/src/Modules/Body/Layout.jsx
@@ -5,6 +5,7 @@ import Footer from "../Footer/Footer";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import { Fab } from "@mui/material";
 import { Link } from "react-router-dom";
+import maintanence from "../../assets/images/maintanence.jpg";
 
 function Layout() {
   const fabStyle = {
@@ -15,18 +16,26 @@ function Layout() {
 
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <Fab
-        color="primary"
-        LinkComponent={Link}
-        to={"/post/create"}
-        sx={fabStyle}
-        aria-label="add"
-      >
-        <PostAddIcon />
-      </Fab>
-      <Footer />
+      {!!process.env.REACT_APP_MAINTANENCE ? (
+        <div style={{ width: "100%", height: "100vh", display: "flex", justifyContent: "center" }}>
+          <img src={maintanence} style={{ width: "45%" }} alt="maintanence" />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+          <Fab
+            color="primary"
+            LinkComponent={Link}
+            to={"/post/create"}
+            sx={fabStyle}
+            aria-label="add"
+          >
+            <PostAddIcon />
+          </Fab>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
