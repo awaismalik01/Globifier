ce21be124570c476b182bfa4336342521860f179
Awais Ahmed Malik <mawais_malik@hotmail.com>
2023-05-18 17:29:23 -0700

    Update index.js
    Check-1

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,32 @@
 const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-var bodyParser = require("body-parser")
+// const cors = require("cors")
+// const mongoose = require("mongoose")
+// var bodyParser = require("body-parser")
 
-require("dotenv").config()
+// require("dotenv").config()
 
 const app = express()
 const port = 4000
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
+// app.use(cors())
+// app.use(express.json())
+// app.use(bodyParser.json())
 
-const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true })
-const connection = mongoose.connection
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully")
-});
+// const uri = process.env.ATLAS_URI
+// mongoose.connect(uri, { useNewUrlParser: true })
+// const connection = mongoose.connection
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully")
+// });
 
-const usersRouter = require("./routes/users")
-const postsRouter = require("./routes/posts")
+// const usersRouter = require("./routes/users")
+// const postsRouter = require("./routes/posts")
 
-app.use("/users", usersRouter)
-app.use("/posts", postsRouter)
+// app.use("/users", usersRouter)
+// app.use("/posts", postsRouter)
 
-const auth = require("./middleware/auth")
-const role = require("./middleware/role")
+// const auth = require("./middleware/auth")
+// const role = require("./middleware/role")
 
 // app.post("/welcome", auth, role(["admin"]), (req, res) => {
 //   res.status(200).send("Welcome 🙌 ")
