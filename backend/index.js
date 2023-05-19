eaf104e17ab5a630d2863f2c41131e74642688a9
Awais Ahmed Malik <mawais_malik@hotmail.com>
2023-05-18 17:41:29 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,25 +12,25 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
 
-const uri = process.env.ATLAS_URI
-// mongoose.connect(uri, { useNewUrlParser: true })
-// const connection = mongoose.connection
-// connection.once("open", () => {
-//   console.log("MongoDB database connection established successfully")
-// });
+const uri = process.env.MONGODB_URI
+mongoose.connect(uri, { useNewUrlParser: true })
+const connection = mongoose.connection
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully")
+});
 
-// const usersRouter = require("./routes/users")
-// const postsRouter = require("./routes/posts")
+const usersRouter = require("./routes/users")
+const postsRouter = require("./routes/posts")
 
-// app.use("/users", usersRouter)
-// app.use("/posts", postsRouter)
+app.use("/users", usersRouter)
+app.use("/posts", postsRouter)
 
-// const auth = require("./middleware/auth")
-// const role = require("./middleware/role")
+const auth = require("./middleware/auth")
+const role = require("./middleware/role")
 
-// app.post("/welcome", auth, role(["admin"]), (req, res) => {
-//   res.status(200).send("Welcome 🙌 ")
-// });
+app.post("/welcome", auth, role(["admin"]), (req, res) => {
+  res.status(200).send("Welcome 🙌 ")
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
