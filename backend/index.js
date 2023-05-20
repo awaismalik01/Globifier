f455da2a0d7f46f9a2a6ba761a6900639be4f2d3
Awais Ahmed Malik <mawais_malik@hotmail.com>
2023-05-20 16:55:12 -0700

    Update index.js
    mongodb

--- backend/index.js
+++ backend/index.js
@@ -41,13 +41,13 @@ app.get('/', (req, res) => {
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//   console.log("Mongodb connected");
+//   server.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+//   });
+// }).catch((err) => {
+//   console.log({ err });
+//   process.exit(1);
+// });
 
